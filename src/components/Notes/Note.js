fbfc09c55d093128b6a0e3e59f7fb2b276094d32
Bonaventure <bonaogeto1@gmail.com>
2023-04-16 22:20:53 +0300

    created Note

---
+++ src/components/Notes/Note.js
@@ -0,0 +1,92 @@
+import "./Notes.css";
+
+export default function Note() {
+  return (
+    <div>
+      <div className="note">
+        <div className="title">Title</div>
+        <div className="text">Take a note...</div>
+        <div className="note-footer">
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">person_add</span>
+            <span className="tooltiptext">Collaborator</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">image</span>
+            <span className="tooltiptext">Add image</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">archive</span>
+            <span className="tooltiptext">Archive</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">more_vert</span>
+            <span className="tooltiptext">More</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">
+              notifications
+            </span>
+            <span className="tooltiptext">Remind me</span>
+          </div>
+        </div>
+      </div>
+      <div className="note">
+        <div className="title">Title</div>
+        <div className="text">Take a note...</div>
+        <div className="note-footer">
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">person_add</span>
+            <span className="tooltiptext">Collaborator</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">image</span>
+            <span className="tooltiptext">Add image</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">archive</span>
+            <span className="tooltiptext">Archive</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">more_vert</span>
+            <span className="tooltiptext">More</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">
+              notifications
+            </span>
+            <span className="tooltiptext">Remind me</span>
+          </div>
+        </div>
+      </div>
+      <div className="note">
+        <div className="title">Title</div>
+        <div className="text">Take a note...</div>
+        <div className="note-footer">
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">person_add</span>
+            <span className="tooltiptext">Collaborator</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">image</span>
+            <span className="tooltiptext">Add image</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">archive</span>
+            <span className="tooltiptext">Archive</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">more_vert</span>
+            <span className="tooltiptext">More</span>
+          </div>
+          <div className="tooltip">
+            <span className="material-symbols-outlined hover">
+              notifications
+            </span>
+            <span className="tooltiptext">Remind me</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
